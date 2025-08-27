4d26a6a70f3af9618507ba3ac68e56d048680879
KyleWhite22 <kewhite900@gmail.com>
2025-08-27 15:42:20 -0400

    fixed ui error

--- frontend/src/ui/AppShell.tsx
+++ frontend/src/ui/AppShell.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { fetchAuthSession, signOut } from "aws-amplify/auth";
 
 export default function AppShell() {
